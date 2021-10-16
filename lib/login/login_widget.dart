7f416a8c81c6f648dd4062e1cea346cffeddfb33
dhruvja <dhruv@iamsizzling.com>
2021-10-16 22:24:02 +0530

    added backend code to login

--- lib/login/login_widget.dart
+++ lib/login/login_widget.dart
@@ -8,6 +8,9 @@ import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 
 class LoginWidget extends StatefulWidget {
   LoginWidget({Key key}) : super(key: key);
@@ -23,6 +26,7 @@ class _LoginWidgetState extends State<LoginWidget> {
   bool _loadingButton1 = false;
   bool _loadingButton2 = false;
   final scaffoldKey = GlobalKey<ScaffoldState>();
+  final _formKey = GlobalKey<FormState>();
 
   @override
   void initState() {
@@ -30,6 +34,52 @@ class _LoginWidgetState extends State<LoginWidget> {
     emailAddressController = TextEditingController();
     passwordController = TextEditingController();
     passwordVisibility = false;
+  }
+
+  var status;
+  var present = false;
+
+  void authorize() async {
+    print(emailAddressController.text);
+    final response =
+        await http.post(Uri.parse('http://localhost:5000/api/login'),
+            headers: <String, String>{
+              'Content-Type': 'application/json; charset=UTF-8',
+            },
+            body: jsonEncode(<String, String>{
+              'email': emailAddressController.text,
+              'password': passwordController.text,
+            }));
+    if (response.statusCode == 200) {
+      // If the server did return a 201 CREATED response,
+      // then parse the JSON.
+      print(response.body);
+      var data = json.decode(response.body);
+      print(data['success']);
+      setState(() {
+        status = data;
+        present = true;
+      });
+    }
+
+    if (present) {
+      print("true");
+      if (status['success']) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Authentication Successful'),backgroundColor: Colors.green),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Authentication is not Successful'),backgroundColor: Colors.redAccent),
+        );
+      }
+    }
+
+    // } else {
+    //   // If the server did not return a 201 CREATED response,
+    //   // then throw an exception.
+    //   throw Exception('Failed to create album.');
+    // }
   }
 
   @override
@@ -51,7 +101,8 @@ class _LoginWidgetState extends State<LoginWidget> {
         ),
         child: Padding(
           padding: EdgeInsetsDirectional.fromSTEB(20, 0, 20, 0),
-          child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
             child: Column(
               mainAxisSize: MainAxisSize.max,
               mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -83,6 +134,12 @@ class _LoginWidgetState extends State<LoginWidget> {
                         child: TextFormField(
                           controller: emailAddressController,
                           obscureText: false,
+                          validator: (value) {
+                            if (value == "") {
+                              return 'Please enter some text';
+                            }
+                            return null;
+                          },
                           decoration: InputDecoration(
                             labelText: 'Email Address',
                             labelStyle: FlutterFlowTheme.bodyText1.override(
@@ -138,6 +195,12 @@ class _LoginWidgetState extends State<LoginWidget> {
                         child: TextFormField(
                           controller: passwordController,
                           obscureText: !passwordVisibility,
+                          validator: (value) {
+                            if (value == "") {
+                              return 'Please enter some text';
+                            }
+                            return null;
+                          },
                           decoration: InputDecoration(
                             labelText: 'Password',
                             labelStyle: FlutterFlowTheme.bodyText1.override(
@@ -146,7 +209,7 @@ class _LoginWidgetState extends State<LoginWidget> {
                               fontSize: 14,
                               fontWeight: FontWeight.normal,
                             ),
-                            hintText: 'Enter your email here...',
+                            hintText: 'Enter your password here...',
                             hintStyle: FlutterFlowTheme.bodyText1.override(
                               fontFamily: 'Lexend Deca',
                               color: Color(0xFF95A1AC),
@@ -209,28 +272,32 @@ class _LoginWidgetState extends State<LoginWidget> {
                                 EdgeInsetsDirectional.fromSTEB(10, 10, 10, 10),
                             child: FFButtonWidget(
                               onPressed: () async {
-                                setState(() => _loadingButton1 = true);
-                                try {
-                                  final user = await signInWithEmail(
-                                    context,
-                                    emailAddressController.text,
-                                    passwordController.text,
-                                  );
-                                  if (user == null) {
-                                    return;
-                                  }
-
-                                  await Navigator.pushAndRemoveUntil(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) =>
-                                          NavBarPage(initialPage: 'HomePage'),
-                                    ),
-                                    (r) => false,
-                                  );
-                                } finally {
-                                  setState(() => _loadingButton1 = false);
+                                // setState(() => _loadingButton1 = true);
+                                // try {
+                                //   final user = await signInWithEmail(
+                                //     context,
+                                //     emailAddressController.text,
+                                //     passwordController.text,
+                                //   );
+                                //   if (user == null) {
+                                //     return;
+                                //   }
+                                if (_formKey.currentState.validate()) {
+                                  print("Validated");
+                                  authorize();
                                 }
+
+                                // await Navigator.pushAndRemoveUntil(
+                                //   context,
+                                //   MaterialPageRoute(
+                                //     builder: (context) =>
+                                //         NavBarPage(initialPage: 'HomePage'),
+                                //   ),
+                                //   (r) => false,
+                                // );
+                                // } finally {
+                                //   setState(() => _loadingButton1 = false);
+                                // }
                               },
                               text: 'Login',
                               options: FFButtonOptions(
